1e9f73896147cd4b984802b067da9634574a5acf
Prateek Madaan <31117403+prateek951@users.noreply.github.com>
2018-06-26 22:15:42 +0530

    added more components and using queryParams

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ErrorHandler } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -8,18 +9,33 @@ import { PostService } from './services/post.service';
 import { AppErrorHandler } from './common/app-error-handler';
 import { GithubFollowersComponent } from './github-followers/github-followers.component';
 import { GithubFollowersService } from './services/github-followers.service';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
+import { GithubProfileComponent } from './github-profile/github-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostsComponent,
-    GithubFollowersComponent
+    GithubFollowersComponent,
+    NavbarComponent,
+    NotFoundComponent,
+    HomeComponent,
+    GithubProfileComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: '',component: HomeComponent },
+      { path: 'followers/:id',component: GithubProfileComponent },
+      { path: 'followers',component: GithubFollowersComponent },
+      { path: 'posts', component: PostsComponent },
+      { path: '**',component: NotFoundComponent }
+    ])
   ],
   providers: [
     PostService,
